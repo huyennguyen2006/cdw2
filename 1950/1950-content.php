a7affeb49d149a1be6cdc05360d72d9faca7aacc
huyennguyen2006 <huyennguyenhcm.up@gmail.com>
2019-05-06 22:47:05 +0700

    update reponsive

--- 1950/1950-content.php
+++ 1950/1950-content.php
@@ -21,20 +21,20 @@
       <span class="right-line"></span>
       </div>
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/1.jpg" class="img-responsive">
             <div class="fade-in">
               <img src="./images/1.jpg" class="img-responsive">
                <div class="product-btn row">
                   <div class="product-list">
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Select Options</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                       </div>
                   </div>
@@ -51,20 +51,20 @@
       
         
       </div>
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/2.jpg" class="img-responsive">
           <div class="fade-in">
             <img src="./images/2-2.jpg" class="img-responsive">
                     <div class="product-btn row">
                    <div class="product-list">
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Add To Carts</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                       </div>
                   </div>
@@ -78,7 +78,7 @@
             </div>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/3.jpg" class="img-responsive">
           <div class="fade-in">
@@ -86,13 +86,13 @@
 
                 <div class="product-btn row">
                   <div class="product-list">
-                  <div class="col-md-4">
+                  <div class="col-md-4 col-sm-4 col-xs-4">
                     <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Select Options</span></i></a>
                   </div>
-                  <div class="col-md-4">
+                  <div class="col-md-4 col-sm-4 col-xs-4">
                     <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                   </div>
-                  <div class="col-md-4">
+                  <div class="col-md-4 col-sm-4 col-xs-4">
                     <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                   </div>
                 </div>
@@ -106,7 +106,7 @@
             </div>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/4.jpg" class="img-responsive">
           <span class="sale">-6%</span>
@@ -114,13 +114,13 @@
             <img src="./images/4-2.jpg" class="img-responsive">
               <div class="product-btn row">
                 <div class="product-list">
-                  <div class="col-md-4">
+                  <div class="col-md-4 col-sm-4 col-xs-4">
                     <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Add To Carts</span></i></a>
                   </div>
-                  <div class="col-md-4">
+                  <div class="col-md-4 col-sm-4 col-xs-4">
                     <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                   </div>
-                  <div class="col-md-4">
+                  <div class="col-md-4 col-sm-4 col-xs-4">
                     <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                   </div>
                 </div>
@@ -138,20 +138,20 @@
     </div>
     <div class="cnt-2">
       <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/5.jpg" class="img-responsive">
             <div class="fade-in">
               <img src="./images/4-2.jpg" class="img-responsive">
                 <div class="product-btn row">
                   <div class="product-list">
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Select Options</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                       </div>
                   </div>
@@ -168,20 +168,20 @@
       
         
       </div>
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/6.jpg" class="img-responsive">
           <div class="fade-in">
             <img src="./images/2-2.jpg" class="img-responsive">
                     <div class="product-btn row">
                  <div class="product-list">
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Add To Carts</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                       </div>
                   </div>
@@ -195,22 +195,21 @@
             </div> 
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/7.jpg" class="img-responsive">
            <span class="sale">-11%</span>
-           <span class="new">New</span>
           <div class="fade-in">
             <img src="./images/5.jpg" class="img-responsive">
                     <div class="product-btn row">
                    <div class="product-list">
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Select Options</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                       </div>
                   </div>
@@ -224,7 +223,7 @@
             </div>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-3 col-xs-12 col-sm-6">
         <div class="nor">
           <img src="./images/2-2.jpg" class="img-responsive">
            <span class="sale">-11%</span>
@@ -232,13 +231,13 @@
             <img src="./images/8-2.jpg" class="img-responsive">
                     <div class="product-btn row">
                   <div class="product-list">
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-shopping-basket"><span class="tooltiptext">Add To Carts</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-files-o"><span class="tooltiptext">Compare</span></i></a>
                       </div>
-                      <div class="col-md-4">
+                      <div class="col-md-4 col-sm-4 col-xs-4">
                         <a href="#"><i class="fa fa-arrows-alt"><span class="tooltiptext">Quick View</span></i></a>
                       </div>
                   </div>
