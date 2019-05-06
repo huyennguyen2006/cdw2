0f95c1fcbab2076fb859cf37ca3885e352971328
huyennguyen2006 <huyennguyenhcm.up@gmail.com>
2019-05-06 21:51:11 +0700

    function grow and drop quatity

--- 1957/js/1957.js
+++ 1957/js/1957.js
@@ -29,4 +29,16 @@ $(document).ready(function(){
   $('.reset_variations').click(function(){
     $b.css('visibility','hidden');
   });
+
+  $('.fa.fa-plus').click(function(){
+    var quantity = $('#quantity_5cc577f043db9').val()
+    $('#quantity_5cc577f043db9').val(parseInt(quantity) + 1)
+  })
+
+  $('.fa.fa-minus').click(()=>{
+    var quantity = $('#quantity_5cc577f043db9').val()
+    if(parseInt(quantity) !== 0) {
+      $('#quantity_5cc577f043db9').val(parseInt(quantity) - 1)
+    }
+  })
 });
