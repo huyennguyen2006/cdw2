21e0c515a41d186324b3f716d70dc1fb82b8bba8
huyennguyen2006 <huyennguyenhcm.up@gmail.com>
2019-05-08 23:44:00 +0700

    layout create account

--- 1958/1958-content.php
+++ 1958/1958-content.php
@@ -8,30 +8,55 @@
         $url_path = str_replace('\\', '/', $url_path);
     ?>
       
-<div class="i-type-1956">
-   <div class="cnt-2">
-      <div class="container">
-         <div class="row">
-            <div class="fix-img col-md-6">
-               <ul class="thmbnails">
-                <li><a href="#slide1"><img src="images/1.jpg"></a></li>
-                <li><a href="#slide2"><img src="images/2.jpg"></a></li>
-                <li><a href="#slide3"><img src="images/3.jpg"></a></li>
-                <li><a href="#slide4"><img src="images/4.jpg"></a></li>
-                <li><a href="#slide5"><img src="images/5.jpg"></a></li>
-              </ul>
-              <div class="img-nails">
-                <ul>
-                <li id="slide1"><img src="images/1-1.jpg"></li>
-                <li id="slide2"><img src="images/2-2.jpg"></li>
-                <li id="slide3"><img src="images/3-3.jpg"></li>
-                <li id="slide4"><img src="images/4-4.jpg"></li>
-                <li id="slide5"><img src="images/5-5.jpg"></li>
-              </ul>
-               <p class="sale-up">-11</p>
-              </div>
-            </div>
-         </div>
+<div class="type-1958">
+   <div class="container">
+    <h2>Personal Information</h2>
+      <form>
+    <div class="form-group row">
+      <div class="col-xs-6">
+        <input class="form-control" id="ex1" type="text" placeholder="First Name">
       </div>
-   </div>
+      <div class="col-xs-6">
+        <input class="form-control" id="ex3" type="text" placeholder="Last Name">
+      </div>
+
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="remember"><span class="sign-up">Sign Up for Newsletter</span></label>
+    </div>
+    <div class="form-group">
+      <label for="inputdefault">Email<span class="star"> * </span></label>
+      <input class="form-control inputdefault" id="inputdefault" type="text">
+    </div>
+    <div class="form-group">
+      <label for="inputdefault">Password<span class="star"> * </span></label>
+      <input class="form-control inputdefault" id="inputdefault" type="text">
+      <div id="ipassword-strength-meter" class="password-strength-meter">Password Strength:
+      <span id="password-strength-meter-label" data-role="password-strength-meter-label">No Password</span>
+    </div>
+    </div>
+
+      <div class="form-group">
+        <label for="inputdefault">Confirm Password<span class="star"> * </span></label>
+        <input class="form-control inputdefault" id="inputdefault" type="text">
+      </div>
+      <div class="btn-element">
+       <button class="col-xs-5 btn btn-back" type="button">Back</button>
+      <button class="col-xs-5 col-xs-offset-2 btn btn-primary" type="button">Create an account</button>
+    </div>
+    </form>
+    <br>
+      <div class="block-content">
+        <div class="actions-toolbar social-btn social-btn-actions-toolbar facebook-login">
+          <a class="btn-block btn-social btn-facebook">
+            <span class="fa fa-facebook"></span>Sign in with Facebook</a>
+        </div>
+        <div class=" actions-toolbar social-btn social-btn-actions-toolbar google-login">
+          <a class="btn-block btn-social btn-google"><span class="fa fa-google"></span>Sign in with Google</a>
+        </div>
+        <div class=" actions-toolbar social-btn social-btn-actions-toolbar twitter-login">
+          <a class="btn-block btn-social btn-twitter"><span class="fa fa-twitter"></span>Sign in with Twitter</a>
+        </div>
+      </div>
+</div>
 </div>
--- 1958/1958.php
+++ 1958/1958.php
@@ -22,16 +22,13 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link href="css/1958.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
-        <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
-        <script type="text/javascript" src="js/swiper.min.js"></script>
-        <script type="text/javascript" src="js/jquery.zoom.js"></script>
-        <script type="text/javascript" src="js/jquery.zoom.min.js"></script>
-        <script src="js/1958.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
     </head>
     <body>
