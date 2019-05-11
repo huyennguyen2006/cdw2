a79c6df50cf5e232c16c7f8f9a383ec72ef3eb76
huyennguyen2006 <huyennguyenhcm.up@gmail.com>
2019-05-11 11:12:27 +0700

    update responsive

--- 1964/1964-content.php
+++ 1964/1964-content.php
@@ -9,26 +9,29 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-1964">
    <div class="container">
-      <div class="row">
-         <div class="col-md-6">
-             <div class="feature">There are 4 products.</div>
-         </div>
-         <div class="col-md-6">
-            <span class="selected"><span class="sort">Sort by:</span><span>
-               <select id="soflow">
-                 <option>Relevance</option>
-                 <option>Name, A to Z</option>
-                 <option>Name, Z to A</option>
-                 <option>Price, low to high</option>
-                 <option>Price, high to low</option>
-                 <option>Price, low to high</option>
-               </select></span></span>
-         </div>
+  <div class="type_test">
+            <div class="row">
+                 <div class="col-md-6">
+                     <div class="feature">There are 4 products.</div>
+                 </div>
+                 <div class="col-md-6">
+                    <span class="selected"><span class="sort">Sort by:</span><span>
+                       <select id="soflow">
+                         <option>Relevance</option>
+                         <option>Name, A to Z</option>
+                         <option>Name, Z to A</option>
+                         <option>Price, low to high</option>
+                         <option>Price, high to low</option>
+                         <option>Price, low to high</option>
+                       </select></span></span>
+                 </div>
       </div>
-      <div class="row">  
-         <div class="col-md-3 col-xl-12 col-sm-12 col-12 ">
-            <div class="a">
-            <img class="hinhsanpham" src="images/designer-sunglasses (1).jpg">
+  </div>
+   <div class="type_potion">
+         <div class="row">  
+         <div class="col-md-3 col-sm-3">
+            <div class="a text-center">
+            <img src="images/designer-sunglasses (1).jpg">
             <div class="new-1">
                <span class="new">NEW</span>
             </div>
@@ -44,8 +47,8 @@ $url_path = str_replace('\\', '/', $url_path);
          </div>
          </div>
          <div class="col-md-3 col-sm-3">
-            <div class="a">
-            <img class="hinhsanpham" src="images/premium-accessories.jpg">
+            <div class="a text-center">
+            <img src="images/premium-accessories.jpg">
        <div class="new-1">
                <span class="new">NEW</span>
             </div>
@@ -61,8 +64,8 @@ $url_path = str_replace('\\', '/', $url_path);
          
          </div>
          <div class="col-md-3 col-sm-3">
-            <div class="a">
-            <img class="hinhsanpham" src="images/plain-casual-shirt.jpg">
+            <div class="a text-center">
+            <img src="images/plain-casual-shirt.jpg">
             <div class="new-1">
                <span class="new">NEW</span>
             </div>
@@ -79,8 +82,8 @@ $url_path = str_replace('\\', '/', $url_path);
          </div>
 
          <div class="col-md-3 col-sm-3">
-            <div class="a">
-            <img class="hinhsanpham" src="images/jewellery-for-women.jpg">
+            <div class="a text-center">
+            <img src="images/jewellery-for-women.jpg">
            <div class="new-1">
                <span class="new">NEW</span>
             </div>
@@ -97,5 +100,6 @@ $url_path = str_replace('\\', '/', $url_path);
          </div>
 
       </div>
+   </div>
    </div>
 </div>
--- 1964/1964.php
+++ 1964/1964.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
     $url_host = 'http://'.$_SERVER['HTTP_HOST'];
     $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
@@ -22,7 +22,7 @@
 <head>
 	<title></title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/1964.css">
